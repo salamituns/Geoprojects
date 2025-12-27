1adeb4c14f8904364d3fa322d06248a7fc5202b7
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 <salamituns@gmail.com>
2025-12-27 00:40:18 -0600

    Enable WAR packaging and Tomcat deployment support
    Updated pom.xml to set packaging to WAR and added Tomcat as a provided dependency for deployment. Modified GeologicalSampleApiApplication to extend SpringBootServletInitializer, enabling deployment to external servlet containers like Tomcat.

--- src/main/java/com/geoscience/sampleapi/GeologicalSampleApiApplication.java
+++ src/main/java/com/geoscience/sampleapi/GeologicalSampleApiApplication.java
@@ -2,6 +2,7 @@ package com.geoscience.sampleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Main application class for the Geological Sample Management API.
@@ -9,9 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * This REST API provides endpoints for managing geological samples collected
  * during field surveys, including rock samples, minerals, soil samples, and
  * other geological specimens.
+ * 
+ * Extends SpringBootServletInitializer to enable WAR deployment to Tomcat.
  */
 @SpringBootApplication
-public class GeologicalSampleApiApplication {
+public class GeologicalSampleApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GeologicalSampleApiApplication.class, args);
